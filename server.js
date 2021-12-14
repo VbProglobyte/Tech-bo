6f9c6b58d40152d4bb6942304af9fe751fc1165d
Val Brisendine <vsbrisendine@gmail.com>
2021-12-14 11:43:33 -0600

    server js file added

---
+++ server.js
@@ -0,0 +1,33 @@
+// dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require('morgan')
+
+// live port connection 
+const PORT = process.envPORT || 3000;
+
+// initialize express 
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// mongoose connection 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// database connection to models
+const db = require("./models");
+// routes for api and html
+app.use(require('./routes/apiRoutes.js'))
+app.use(require('./routes/htmlRoutes.js'))
+
+// /////////////////////////////////////////////////////
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
