b8d69091a4a0b3e36d235dd89359704e0e63f146
Val Brisendine <vsbrisendine@gmail.com>
2021-12-15 09:11:14 -0600

    trying to fix the mongodb connection looking at typos quotes and shell issues

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // dependencies
 const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require('morgan')
+// const morgan = require("morgan")
 // /////////////////////////////////////////
 // live port connection 
 const PORT = process.envPORT || 3000;
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 // mongoose connection - aquired from seed file for mongo // heroku connection  - murmuring-taiga-99726
 // mongodb+srv://VbProglobyte:Vb784839@cluster0.5alc9.mongodb.net/tech-bo?retryWrites=true&w=majority
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/tech-bo", 
+  process.env.MONGODB_URI || 'mongodb://localhost/tech-bo', 
   {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -32,11 +32,11 @@ mongoose.connect(
 // database connection to models
 // const db = require("./models");
 
-// routes for api and html
-app.use(require('./routes/apiRoutes.js'))
-app.use(require('./routes/homeRoutes.js'))
+// routes for api and html - connection probs may be quote related (?)
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/homeRoutes.js"));
 
-// /////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////// 3000
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
